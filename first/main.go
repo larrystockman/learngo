0ac2bc3c5db9087906b06446f397358d4f08c200
larrystockman <larrystockman@outlook.com>
2019-01-30 21:16:05 -0600

    Updated my BFF

--- first/main.go
+++ first/main.go
@@ -4,6 +4,13 @@ import (
 	"fmt"
 )
 
+var me = "Lawrence Stockman"
+var bf = "Maggie Stockman"
+
 func main() {
-	fmt.Println("Lawrence Stockman", "Maggie Stockman")
+	var me = "Lawrence Stockman"
+	var bf = "Maggie Stockman"
+
+	fmt.Println("I am:", me)
+	fmt.Println("My best friend is:", bf)
 }
